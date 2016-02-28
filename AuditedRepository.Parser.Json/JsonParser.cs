7d3ea3528189089a9a3f829d4af0e89c86fe697d
Daniel Charlton <charlton.dan@reiversolutions.co.uk>
2016-02-28 14:19:03 +0000

    JsonParser skeleton added to project

---
+++ AuditedRepository.Parser.Json/JsonParser.cs
@@ -0,0 +1,18 @@
+﻿using AuditedRepository.Interfaces.Models;
+using AuditedRepository.Interfaces.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuditedRepository.Parser.Json
+{
+    public class JsonParser<T> : IParser<T> where T : class, IEntity
+    {
+        public string Parse(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
