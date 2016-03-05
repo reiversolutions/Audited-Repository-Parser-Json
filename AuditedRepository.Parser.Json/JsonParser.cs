4aff67a739ad16dc9ff03c93671c62e66d5a3f27
Daniel Charlton <charlton.dan@reiversolutions.co.uk>
2016-03-05 15:18:25 +0000

    Parser updated to handle updates from core AuditedRepository

--- AuditedRepository.Parser.Json/JsonParser.cs
+++ AuditedRepository.Parser.Json/JsonParser.cs
@@ -1,6 +1,7 @@
 ﻿using AuditedRepository.Interfaces.Models;
 using AuditedRepository.Interfaces.Parsers;
 using AuditedRepository.Parsers;
+using AuditedRepository.Validators;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -10,10 +11,13 @@ using System.Threading.Tasks;
 
 namespace AuditedRepository.Parser.Json
 {
-    public class JsonParser<T> : ParserBase<T>, IParser<T> where T : class, IEntity
+    public class JsonParser<T> : ParserBase<T>, IParser<T> where T : IEntity
     {
         public override string Parse(T entity)
         {
+            var validator = new EntityValidator<T>();
+            validator.Validate(entity);
+
             return JsonConvert.SerializeObject(entity);
         }
     }
--- Tests.Parser.Json/ParserTests.cs
+++ Tests.Parser.Json/ParserTests.cs
@@ -1,15 +1,70 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AuditedRepository.Parser.Json;
+using AuditedRepository.Interfaces.Models;
+using AuditedRepository.Interfaces.Parsers;
+using AuditedRepository.Models;
 
 namespace Tests.Parser.Json
 {
     [TestClass]
     public class ParserTests
     {
-        [TestMethod]
-        public void ParseBaseClass()
+        private IEntity EMPTY_ENTITY = new Entity();
+        private IEntity BASIC_ENTITY = new Entity()
         {
+            Id = Guid.NewGuid().ToString(),
+            CreatedDate = DateTime.Now,
+            ModifiedDate = DateTime.Now,
+            IsArchived = false
+        };
 
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ParseEntity_null()
+        {
+            // Assign
+            IParser<IEntity> parser = new JsonParser<IEntity>();
+            IEntity entity = null;
+
+            // Act
+            var result = parser.Parse(entity);
+
+            // Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ParseEntity_Empty()
+        {
+            // Assign
+            IParser<IEntity> parser = new JsonParser<IEntity>();
+
+            // Act
+            var jsonResult = parser.Parse(EMPTY_ENTITY);
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void ParseEntity_Basic()
+        {
+            // Assign
+            IParser<IEntity> parser = new JsonParser<IEntity>();
+            string jsonExpected = string.Format(
+                "\"Id\":\"{0}\",\"IsArchived\":{1},\"ModifiedDate\":\"{2}\",\"CreatedDate\":\"{3}\"",
+                BASIC_ENTITY.Id,
+                BASIC_ENTITY.IsArchived.ToString().ToLower(),
+                BASIC_ENTITY.ModifiedDate.ToString("yyyy-MM-ddTHH:mm:ss.fffffff+00:00"),
+                BASIC_ENTITY.CreatedDate.ToString("yyyy-MM-ddTHH:mm:ss.fffffff+00:00")
+            );
+            jsonExpected = "{" + jsonExpected + "}";
+
+            // Act
+            var jsonResult = parser.Parse(BASIC_ENTITY);
+
+            // Assert
+            Assert.AreEqual(jsonExpected, jsonResult);
         }
     }
 }
